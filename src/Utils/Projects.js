218fe1a0862d676051083d9544597cb1051367ef
Vishaltandale987 <tandalevishal987@gmail.com>
2023-01-11 01:40:02 +0530

    add

---
+++ src/Utils/Projects.js
@@ -0,0 +1,12 @@
+export const projects = [
+  {
+    img: "https://user-images.githubusercontent.com/107978823/207076321-85d24bad-0669-49b4-929f-f65005e7b915.png",
+    title: "Clone of Modesens",
+    des: "This is a clone of the Modesens E-Commerce website. The clone is complete with a Login page, Sign-in page, Pricing Page, CaseStudies and EnterPrise. Following is the link to the clone of the website.",
+    demo: "",
+    live: "https://zesty-tapioca-5b88fe.netlify.app",
+    gitHub: "https://github.com/surya382/Modesens.com",
+    technologies: ["Java Script,", "HTML,", "CSS,"],
+  },
+
+];
